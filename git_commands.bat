2c612d35c52079a814f9dad91f3695dfedc865b3
Avaneesh Singh <avaneeshs@vmware.com>
2016-01-20 12:53:27 +0530

    adding src folder from batch file

---
+++ git_commands.bat
@@ -0,0 +1,6 @@
+git checkout master
+git status
+git add .
+git commit -m "adding src folder from batch file"
+git remote add cloudOps https://manishyadav867:18tilldie@github.com/manishyadav867/MainRepo.git
+git push -u cloudOps master
