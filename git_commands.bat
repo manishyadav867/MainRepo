eacb035ef6a1d2ba45a9753942c1acbc44949569
Tripathy <ratnakar.a.tripathy@accenture.com>
2016-01-29 19:05:41 +0530

    adding src folder from batch file

--- git_commands.bat
+++ git_commands.bat
@@ -4,3 +4,5 @@ git add .
 git commit -m "adding src folder from batch file"
 git remote add cloudOps https://manishyadav867:18tilldie@github.com/manishyadav867/MainRepo.git
 git push -u cloudOps master
+
+prompt
